0577ed61a428ba7fd8aabb19bd7d744e53769d6d
jjmajack@gmail.com <jjmajack@gmail.com>
2020-01-27 19:32:56 +0200

    New project

---
+++ app/src/main/java/com/example/jackson/answerme/Grade.java
@@ -0,0 +1,37 @@
+package com.example.jackson.answerme;
+
+/**
+ * Created by DELL on 2020-01-25.
+ */
+
+public class Grade {
+    private int gradeId;
+    private String grade;
+
+    public Grade(int gradeId, String grade) {
+        this.gradeId = gradeId;
+        this.grade = grade;
+    }
+
+    public int getGradeId() {
+        return gradeId;
+    }
+
+    public void setGradeId(int gradeId) {
+        this.gradeId = gradeId;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString(){
+        return grade;
+    }
+
+}
